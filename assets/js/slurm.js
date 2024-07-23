0b526ef9d0ec0ea40dd2ec349d458d3f9aced597
Oloyede2001 <Jesutomisinoloyede@gmail.com>
2024-07-23 11:24:33 -0400

    Commented

--- assets/js/slurm.js
+++ assets/js/slurm.js
@@ -535,7 +535,8 @@
         var selectedString;
         var runtimeHr = $('#runtimeHr');
         runtimeHr.empty();
-
+        
+        /*
         // Populate days dropdown
         var runtimeDays = $('#runtimeDays');
         runtimeDays.empty();
@@ -543,6 +544,7 @@
         selectedString = (d == 0) ? " selected" : "";
          runtimeDays.append('<option value="' + d + '"' + selectedString + '>' + d + '</option>');
       }
+        */
         for (j = 0; j <= runtimeMaxHour; j++) {
           selectedString = (j == runtimeDefault) ? " selected" : "";
           //handle single-digit numbers
